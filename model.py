61401b023ac3361fa6579d28b986c92d33ea9155
Abrehg <adityaasuratkal@gmail.com>
2023-06-30 19:36:53 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,17 @@ from tensorflow import keras as keras
 from keras import layers as tfl
 from inputFormatting import formatImg
 from Encoder import encoder
+from formatText import formatText
+from decodeFormat import decodeFormat
+from Decoder import decoder
 
-def model(image):
+def model(image, text):
+    #input encoding of image (Transformer encoder + MAE)
     X = formatImg(image)
     X = encoder(X)
+    #input encoding of text (CLIP)
+    Q = formatText(text)
+    #decode and return output mask (Transformer decoder)
+    X = decodeFormat(X, Q)
+    mask = decoder(X)
+    return mask
