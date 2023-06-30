1d271c2adcc188f1f1968d2f5a57220324f4cd99
Abrehg <adityaasuratkal@gmail.com>
2023-06-30 20:32:06 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,13 +8,15 @@ from formatText import formatText
 from decodeFormat import decodeFormat
 from Decoder import decoder
 
-def model(image, text):
-    #input encoding of image (Transformer encoder + MAE)
-    X = formatImg(image)
-    X = encoder(X)
-    #input encoding of text (CLIP)
-    Q = formatText(text)
-    #decode and return output mask (Transformer decoder)
-    X = decodeFormat(X, Q)
-    mask = decoder(X)
-    return mask
+image = keras.Input(shape=(None, None, 3))
+text = keras.Input(shape=(None))
+
+#input encoding of image (Transformer encoder + MAE)
+X = formatImg(image)
+X = encoder(X)
+#input encoding of text (CLIP)
+Q = formatText(text)
+#decode and return output mask (Transformer decoder)
+X = decodeFormat(X, Q)
+mask = decoder(X)
+model = keras.Model(inputs = [image, text], outputs = mask)
