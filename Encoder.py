8724d76c26056366093b1f34af39e79395c63015
Abrehg <adityaasuratkal@gmail.com>
2023-07-07 18:52:22 +0530

    Completed Encoder architecture, fine tuning hyperparameters

--- Encoder.py
+++ Encoder.py
@@ -4,12 +4,12 @@ from tensorflow import keras as keras
 from keras import layers as tfl
 from inputFormatting import formatImg
 
-encLayers = 25
-NNLayers = 3
-units = 10
+encLayers = 30
+NNLayers = 10
+units = 512
 
-imgFilePath = "Test"
-#imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/Indoor Semantic Segmentation/images/vedioDataCollection_July2019_Kent0001.png"
+#imgFilePath = "Test"
+imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/Indoor Semantic Segmentation/images/vedioDataCollection_July2019_Kent0001.png"
 #imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/ADEChallengeData2016/images/training/ADE_train_00000001.jpg"
 
 fullEncodings, MAEencodings, MAEpositions = formatImg(imgFilePath)
@@ -21,12 +21,12 @@ def feedForwardNN(baseInput):
     return X
 
 def encoderLayer(input):
-    X = tfl.MultiHeadAttention(num_heads= 16)(input)
+    X = tfl.MultiHeadAttention(num_heads = 16, key_dim = 25, dropout=0.10)(input, input)
     X = tfl.Add()([X, input])
-    input2 = tfl.LayerNormalization()(X)
+    input2 = tfl.LayerNormalization(axis = 1)(X)
     X = feedForwardNN(X)
     X = tfl.Add()([X, input])
-    output = tfl.LayerNormalization()(X)
+    output = tfl.LayerNormalization(axis = 1)(X)
     return output
 
 def encoder(input):
@@ -34,3 +34,8 @@ def encoder(input):
     for i in range(0, encLayers):
         X = encoderLayer(X)
     return X
+
+out = encoder(MAEencodings)
+fullModel = keras.Model(inputs = [MAEencodings, MAEpositions], outputs = out)
+print(fullModel.summary())
+print(out)
