079d44a02677dfca4d61ceba33f5c59646bb931d
Abrehg <adityaasuratkal@gmail.com>
2023-08-02 21:39:36 -0400

    Finished creating full NLP model to train encoder

--- trainCLIP.py
+++ trainCLIP.py
@@ -4,8 +4,27 @@ from keras import layers as tfl
 from formatText import formatText
 from CLIPencoder import textEncoder
 
-textInput = 'Hello World'
+#textInput = 'Hello World'
 
-embeddings = formatText(textInput)
-out = textEncoder()(embeddings)
-print(tf.shape(out))
+#embeddings = formatText(textInput)
+
+NNLayers = 4
+unitsMid = 1000
+unitsOut = 1
+
+inputEmbeddings = keras.Input((None, 300))
+textEnc = textEncoder()
+x = textEnc(inputEmbeddings)
+
+#x = tfl.GlobalAveragePooling1D()(x)
+for i in range(NNLayers):
+    x = tfl.Dense(unitsMid, activation='relu')(x)
+output = tfl.Dense(unitsOut, activation='softmax')(x)
+
+# Create the combined model
+combined_model = keras.Model(inputs=inputEmbeddings, outputs=output)
+
+combined_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Print the model summary
+combined_model.summary()
