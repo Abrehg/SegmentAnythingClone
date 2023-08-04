a7990b28bed76f77997a5f3df3cae1a665e29254
Abrehg <adityaasuratkal@gmail.com>
2023-08-04 18:07:52 -0400

    Improved efficiency of format text function

--- formatText.py
+++ formatText.py
@@ -3,11 +3,12 @@ from gensim.models import KeyedVectors
 import string
 import numpy as np
 
+glove_file = '/Users/adityaasuratkal/Downloads/glove.840B.300d.txt'
+word_vectors = KeyedVectors.load_word2vec_format(glove_file, binary=False, no_header=True)
+
 def formatText(text):
     text = remove_punctuation(text)
     slicedInput = text.split()
-    glove_file = '/Users/adityaasuratkal/Downloads/glove.840B.300d.txt'
-    word_vectors = KeyedVectors.load_word2vec_format(glove_file, binary=False, no_header=True)
     embeddings = [word_vectors[word] for word in slicedInput if word in word_vectors]
     Embedding = tf.convert_to_tensor(embeddings, dtype=tf.float32)
     finalEmbedding = tf.expand_dims(Embedding, axis=0)
