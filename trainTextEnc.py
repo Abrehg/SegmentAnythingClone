bc594feb29c005b0ad86aa27c41de75c5fb4832f
Abrehg <adityaasuratkal@gmail.com>
2023-08-08 17:57:51 -0400

    further fixes to Text Encoder

--- trainTextEnc.py
+++ trainTextEnc.py
@@ -3,7 +3,7 @@ from tensorflow import keras as keras
 from keras import layers as tfl
 from formatText import formatText
 from formatText import findWord
-from CLIPencoder import textEncoder
+from TextEncoder import textEncoder
 import tensorflow_datasets as tfds
 
 """
@@ -20,6 +20,7 @@ unitsOut = 1
 
 eos_token = tf.zeros((1, 300))
 
+#positional encoding function
 def positional_encoding(inputs):
     seq_len, d_model = tf.shape(inputs)[1], inputs.shape[-1]
     position = tf.range(start=0, limit=seq_len, delta=1, dtype=tf.float32)
@@ -32,6 +33,7 @@ def positional_encoding(inputs):
     angle_rads = tf.expand_dims(angle_rads, axis=0)
     return angle_rads
 
+#Single decoder layer (typical layer used for chat GPT, BERT, and more)
 def transformer_decoder_layer(inputs, enc_output):
     pos_encodings = positional_encoding(inputs)
     vec = inputs + pos_encodings
@@ -52,28 +54,44 @@ def generate_sequence(encoder_output, start_token):
     sequence = [start_token]
     max_length = 2
 
+    #sequence generation loop
     for i in range(max_length):
+        #convert input to tensor
         sequence_tensor = tf.expand_dims(tf.concat(sequence, axis=0), axis=0)
+
+        #generate single output in time for sequence
         decoder_output = transformer_decoder_layer(sequence_tensor, encoder_output)
+
+        #condense large output into size (1, 300)
         next_token = tfl.GlobalAveragePooling1D()(decoder_output)
+
+        #create a way to use the variable (eos_token) as a flag in order to stop the sequence from generating further
+        #essentially, if eos_token == next_token: break
+        #but next_token is a blank input tensor when initiated with a model, which can't be used with a regular tensor in a flag based system
 
         # Append the token to the sequence
         sequence.append(next_token)
 
+    # Remove the start_token
     sequence = sequence[1:]
+
+    #combine list into tensor
     sequence = tf.stack(sequence, axis=1)
-      # Remove the start_token
 
     return sequence
 
-# encoder output
+# input tensor
 inputEmbeddings = keras.Input((None, 300))
+
+#generate encoder output (works)
 textEnc = textEncoder()
 encoder_output = textEnc(inputEmbeddings)
+
+# condense encoder output into a start token
 X = tfl.GlobalAveragePooling1D()(encoder_output)
 start_token = tfl.Dense(300, 'relu')(X)
 
-# Generate a sequence using autoregressive decoding
+# Generate a sequence using transformer decoder
 generated_sequence = generate_sequence(encoder_output, start_token)
 
 print("Generated Sequence:", generated_sequence)
@@ -87,7 +105,7 @@ combined_model.compile(optimizer='adam', loss='categorical_crossentropy', metric
 combined_model.summary()
 
 """
-#sentiment analysis training
+#text summarization training
 dataset_name = 'multi_news'
 (train_data, val_data, test_data), info = tfds.load(dataset_name, split=['train', 'validation', 'test'], shuffle_files=True, with_info=True)
 print(info)
@@ -108,15 +126,18 @@ y_test = process_dataset(test_data, 'summary')
 batch_size = 512
 num_epochs = 10
 
+#train dataset
 combined_model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=num_epochs)
 
 combined_model.fit(x=x_val, y=y_val, batch_size=batch_size, epochs=num_epochs)
 
 combined_model.test_on_batch(x=x_test, y=y_test)
 
+#save desired weights
 textEnc.save_weights('./text_encoder_weights.h5')
 """
 """
+#develop loss function based on beam search that also slightly adds in loss for longer sequences
 with tf.GradientTape() as tape:
     predictions = model(inputs)
     loss = loss_fn(targets, predictions)
