bd69a7dc8de497607e22a667e9cdc8ca7718f889
Abrehg <adityaasuratkal@gmail.com>
2023-08-27 23:35:27 -0400

    Finished decoder with working architecture

--- Decoder.py
+++ Decoder.py
@@ -13,24 +13,36 @@ imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/Indoor Sem
 
 fullEncodings, MAEencodings, shape = formatImg(formatTensorFromPath(imgFilePath))
 enc = imgEncoder()
-output = enc(MAEencodings)
+encodings = enc(fullEncodings)
 
 textInput = 'Hello World this is a test'
 embeddings = formatText(textInput)
 
-
-#return final mask with correct image shape using FCNN, MHA, and maybe reverse CNNs
-
 def decoder(text, image, shape):
-    image = tfl.MultiHeadAttention(num_heads=18, key_dim=1024, dropout=0.3)(image,  image)
+    image = tfl.MultiHeadAttention(num_heads=18, key_dim=1024, dropout=0.3)(image, image)
     text = tfl.Dense(1024, 'relu')(text)
-    X = tfl.Add()([image, text])
-    X = tfl.Normalization()(X)
+    combined = tf.matmul(image, text, transpose_b=True)
+    combined_features = tf.matmul(combined, text)
+    X = tfl.LayerNormalization()(combined_features)
+    print(tf.shape(X))
     X = tfl.MultiHeadAttention(num_heads=20, key_dim=1024, dropout=0.3)(X, X)
-    X = tfl.Dense(512)(X)
-    return X
+    
+    # Reshape the output tensor to match the desired spatial dimensions
+    X_reshaped = tf.reshape(X, [-1, shape[1] // 16, shape[2] // 16, 1024])
+    print(tf.shape(X_reshaped))
 
+    # Upsample the spatial dimensions using transposed convolutional layers
+    X_upsampled = tfl.Conv2DTranspose(512, (3, 3), strides=(2, 2), padding='same', activation='relu')(X_reshaped)
+    print(tf.shape(X_upsampled))
+    X_upsampled = tfl.Conv2DTranspose(256, (3, 3), strides=(2, 2), padding='same', activation='relu')(X_upsampled)
+    print(tf.shape(X_upsampled))
+    X_upsampled = tfl.Conv2DTranspose(128, (3, 3), strides=(2, 2), padding='same', activation='relu')(X_upsampled)
+    print(tf.shape(X_upsampled))
+    output = tfl.Conv2DTranspose(1, (3, 3), strides=(2, 2), padding='same', activation='sigmoid')(X_upsampled)
+    print(tf.shape(output))
+    
+    return output
 
 print(shape)
-out = decoder(embeddings, fullEncodings, shape)
+out = decoder(embeddings, encodings, shape)
 print(tf.shape(out))
