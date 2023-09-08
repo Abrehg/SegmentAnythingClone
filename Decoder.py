4db3a1b25d7641946f59f841bf2e898b53fc2d82
Abrehg <adityaasuratkal@gmail.com>
2023-09-08 13:31:44 -0400

    Final steps to a full working model

--- Decoder.py
+++ Decoder.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import optuna
 from tensorflow import keras as keras
 from keras import layers as tfl
 from formatImg import formatTensorFromPath
@@ -9,50 +8,52 @@ from formatText import formatText
 from TextEncoder import textEncoder
 
 #imgFilePath = "Test"
-imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/Indoor Semantic Segmentation/images/vedioDataCollection_July2019_Kent0001.png"
-#imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/ADEChallengeData2016/images/training/ADE_train_00000001.jpg"
-fullEncodings, MAEencodings, shape = formatImg(formatTensorFromPath(imgFilePath))
-print(tf.shape(fullEncodings))
+imgFilePath = "/Users/adityaasuratkal/Downloads/Img_Data/Indoor Semantic Segmentation/images/vedioDataCollection_July2019_Kent0001.png"
+#imgFilePath = "/Users/adityaasuratkal/Downloads/Img_Data/ADEChallengeData2016/images/training/ADE_train_00000001.jpg"
+fullEncodings, MAEencodings, height, width = formatImg(formatTensorFromPath(imgFilePath))
 imgEnc = imgEncoder()
 imgEncodings = imgEnc(fullEncodings)
+heights = [height]
+widths = [width]
 
 textInput = 'Hello World this is a test'
 embeddings = formatText(textInput)
 textEnc = textEncoder()
 textEncodings = textEnc(embeddings)
 
-def decoder(text, image, shape):
+def decoder(text, image, height, width):
     image = tfl.MultiHeadAttention(num_heads=18, key_dim=1024, dropout=0.3)(image, image)
     text = tfl.Dense(1024, activation='relu')(text)
-    
-    # Accessing height and width values from the shape tensor
-    height = shape[:, 0]
-    width = shape[:, 1]
     
     combined = tf.matmul(image, text, transpose_b=True)
     combined_features = tf.matmul(combined, text)
     X = tfl.LayerNormalization()(combined_features)
     X = tfl.MultiHeadAttention(num_heads=20, key_dim=1024, dropout=0.3)(X, X)
     
-    # Reshape the output tensor to match the desired spatial dimensions for the entire batch
-    X_reshaped = tf.reshape(X, [-1, height[0] // 16, width[0] // 16, 1024])  # Using height[0] as an example
-    
+    # Reshape each output tensor in the batch to its specified spatial dimensions
+    X = tf.reshape(X, [-1, tf.cast(height // 16, tf.int32), tf.cast(width // 16, tf.int32), 1024])
+
     # Upsample the spatial dimensions using transposed convolutional layers
-    X_upsampled = tfl.Conv2DTranspose(512, (3, 3), strides=(2, 2), padding='same', activation='relu')(X_reshaped)
+    X_upsampled = tfl.Conv2DTranspose(512, (3, 3), strides=(2, 2), padding='same', activation='relu')(X)
     X_upsampled = tfl.Conv2DTranspose(256, (3, 3), strides=(2, 2), padding='same', activation='relu')(X_upsampled)
     X_upsampled = tfl.Conv2DTranspose(128, (3, 3), strides=(2, 2), padding='same', activation='relu')(X_upsampled)
     output = tfl.Conv2DTranspose(1, (3, 3), strides=(2, 2), padding='same', activation='sigmoid')(X_upsampled)
     return output
 
+out = decoder(textEncodings, imgEncodings, heights[0], widths[0])
+
+"""
 # Define your input layers
 img = keras.Input(shape=(None, 1024))  # Assuming the None dimension is the batch size
 text = keras.Input(shape=(None, 300))  # Assuming the None dimension is the batch size
-imgShape = keras.Input(shape=(4))  # Assuming the None dimension is the batch size
+imgShape = keras.Input(shape=(2))  # Assuming the None dimension is the batch size
 
 # Build the decoder model
 imgEncode = imgEnc(img)
 textEncode = textEnc(text)
-output = decoder(textEncode, imgEncode, imgShape)
+shapes = [[1000, 2000], [1000, 2000], [300, 400]]
+output = decoder(textEncodings, imgEncodings, shape)
 
-fullModel = keras.Model(inputs=[img, text, imgShape], outputs=output)
-fullModel.summary()
+#fullModel = keras.Model(inputs=[img, text, imgShape], outputs=output)
+#fullModel.summary()
+"""
--- model.py
+++ model.py
@@ -10,39 +10,62 @@ from formatText import findWord
 from TextEncoder import textEncoder
 from Decoder import decoder
 
-#encode text inputs using the CLIP architecture
-#Then figure out how to decode into a full workable mask with lightweight decoder using MAE and CLIP 
-#Finally train the decoder network to generate a workable mask using the Meta Segment Anything dataset
-
 #imgFilePath = "Test"
-imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/Indoor Semantic Segmentation/images/vedioDataCollection_July2019_Kent0001.png"
-#imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/ADEChallengeData2016/images/training/ADE_train_00000001.jpg"
+imgFilePath = "/Users/adityaasuratkal/Downloads/Img_Data/Indoor Semantic Segmentation/images/vedioDataCollection_July2019_Kent0001.png"
+#imgFilePath = "/Users/adityaasuratkal/Downloads/Img_Data/ADEChallengeData2016/images/training/ADE_train_00000001.jpg"
 text = "Hello World"
 
 image = formatTensorFromPath(imgFilePath)
 #input encoding of image (Transformer encoder + MAE)
-fullEncodings, MAEencodings, shape = formatImg(image)
-X = imgEncoder()(MAEencodings)
+#fullEncodings, MAEencodings, height, width = formatImg(image)
+heights = keras.layers.Input((1))
+widths = keras.layers.Input((1))
+imgEncodings = keras.layers.Input((None, 1024))
+X = imgEncoder()(imgEncodings)
 
 #input encoding of text (CLIP)
-Q = formatText(text)
+#Q = formatText(text)
+textEncodings = keras.layers.Input((None, 300))
 textEnc = textEncoder()
-Q = textEnc(Q)
+Q = textEnc(textEncodings)
 
 #decode and return output mask (Transformer decoder)
-#X = decodeFormat(X)
-#mask = decoder(X)
-#model = keras.Model(inputs = image, outputs = mask)
-#print(model.summary())
+masks = []
+num_elements = tf.shape(Q)[0]
+
+# Convert num_elements_int to a NumPy integer
+num_elements_int = tf.reduce_prod(num_elements)
+
+if num_elements_int == None:
+    num_elements_int = 1
+
+def process_input(i):
+    mask = decoder(
+        Q[i], 
+        X[i], 
+        heights[i], 
+        widths[i]
+    )
+    return mask
+
+for ctr in range(num_elements_int):
+    masks.append(process_input(ctr))
+
+outputMasks = tf.concat(masks, axis = 0)
+
+model = keras.Model(inputs = [imgEncodings, textEncodings, heights, widths], outputs = outputMasks)
+print(model.summary())
 
 #imgFilePath = "Test"
-imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/Indoor Semantic Segmentation/images/vedioDataCollection_July2019_Kent0001.png"
-#imgFilePath = "/Users/adityaasuratkal/Downloads/ML_Projects/UNet/Data/ADEChallengeData2016/images/training/ADE_train_00000001.jpg"
+imgFilePath = "/Users/adityaasuratkal/Downloads/Img_Data/Indoor Semantic Segmentation/images/vedioDataCollection_July2019_Kent0001.png"
+#imgFilePath = "/Users/adityaasuratkal/Downloads/Img_Data/ADEChallengeData2016/images/training/ADE_train_00000001.jpg"
 text = "Hello World"
 
-fullEncodings, MAEencodings = formatImg(formatTensorFromPath(imgFilePath))
-X = imgEncoder()(MAEencodings)
+fullEncodings, MAEencodings, height, width = formatImg(formatTensorFromPath(imgFilePath))
+X = imgEncoder()(fullEncodings)
 
 Q = formatText(text)
 textEnc = textEncoder()
 Q = textEnc(Q)
+
+out = decoder(Q, X, height, width)
