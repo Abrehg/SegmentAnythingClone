b077ebf550d712d315c9c37bf6592f0ebf4c71cc
Abrehg <adityaasuratkal@gmail.com>
2023-10-30 16:40:25 -0400

    Finalizing Model object and importing to jupyter notebook

--- model.py
+++ model.py
@@ -8,8 +8,6 @@ from formatText import formatText
 from TextEncoder import textEncoder
 from Decoder import decoder
 
-
-
 #input encoding of image (Transformer encoder + MAE)
 imgEncodings_input = keras.Input((None, None, 1024), name="img_encodings")
 imgEnc = imgEncoder()
@@ -24,7 +22,7 @@ Q = textEnc(textEncodings_input)
 masks = decoder(Q, X)
 
 model = keras.Model(inputs = [imgEncodings_input, textEncodings_input], outputs = masks)
-
+"""
 imgFilePath2 = "/Users/adityaasuratkal/Downloads/Img_Data/ADE20K/ADE_frame_00000007/X/ADE_frame_00000007.jpg"
 text = "Hello World"
 img = formatTensorFromPath(imgFilePath2)
@@ -37,7 +35,7 @@ out = model.predict([fullImgEncodings, textEncodings])
 
 mask = tf.io.read_file("/Users/adityaasuratkal/Downloads/Img_Data/ADE20K/ADE_frame_00000007/Y/instance_018_ADE_frame_00000007.png")
 correct = tf.io.decode_image(mask, channels=1, dtype=tf.dtypes.uint8)
-
+"""
 def custom_loss(y_true, y_pred):
     epsilon = 0.3
     epsilonTensorTrue = tf.cast(epsilon, y_true.dtype)
@@ -61,11 +59,11 @@ def custom_loss(y_true, y_pred):
 
     return loss
 
-print(f"model output shape: {tf.shape(out[0])}")
-print(f"mask shape: {tf.shape(correct)}")
+#print(f"model output shape: {tf.shape(out[0])}")
+#print(f"mask shape: {tf.shape(correct)}")
 
-print(custom_loss(correct, out[0]))
+#print(custom_loss(correct, out[0]))
 
 model.compile(optimizer= 'adam', loss=custom_loss)
 
-print("model compiled")
+#print("model compiled")
